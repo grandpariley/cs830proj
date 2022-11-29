ead0d8b3b1629fd02b35dc628c1367c992fdf671
grandpariley <rileyherman@tuta.io>
2022-11-28 20:05:14 -0600

    write results

--- deeplearning/crazy.py
+++ deeplearning/crazy.py
@@ -4,18 +4,20 @@ import numpy as np
 import nlp
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import json
 
 dataset = nlp.load_dataset('emotion')
 (x, y), (_, _) = tfds.as_numpy(tfds.load(
-        'ag_news_subset',
-        split=['train', 'test'],
-        batch_size=-1,
-        as_supervised=True,
-    ))
+    'ag_news_subset',
+    split=['train', 'test'],
+    batch_size=-1,
+    as_supervised=True,
+))
 
 
 def trim(b):
     return b.decode('utf-8').lower().replace("\\", " ")
+
 
 x = [trim(i) for i in x]
 
@@ -25,12 +27,12 @@ new_dataset = nlp.dataset_dict.DatasetDict({
         'label': y[:10000]
     },
     'validation': {
-        'text': x[10000:20000],
-        'label': y[10000:20000]
+        'text': x[10000:11000],
+        'label': y[10000:11000]
     },
     'test': {
-        'text': x[20000:21000],
-        'label': y[20000:21000]
+        'text': x[11000:12000],
+        'label': y[11000:12000]
     }
 })
 
@@ -77,8 +79,7 @@ def names_to_ids(labels): return np.array(
     [class_to_index.get(l) for l in labels])
 
 
-train_labels = names_to_ids(labels)
-padded_train_seq = get_sequences(tokenizer, descriptions)
+
 
 model = tf.keras.models.Sequential([tf.keras.layers.Embedding(10000, 16, input_length=maxlen), tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(
     20, return_sequences=True)), tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(20)), tf.keras.layers.Dense(4, activation='softmax')])
@@ -88,10 +89,19 @@ model.compile(loss='sparse_categorical_crossentropy',
 val_descriptions, val_labels = get_description(val)
 val_seq = get_sequences(tokenizer, val_descriptions)
 val_labels = names_to_ids(val_labels)
-h = model.fit(padded_train_seq, train_labels, validation_data=(val_seq, val_labels),
-              epochs=20, callbacks=[tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=2)])
+results = []
+for b in range(5):
+    train_labels = names_to_ids(labels)
+    padded_train_seq = get_sequences(tokenizer, descriptions)
+    h = model.fit(padded_train_seq, train_labels, validation_data=(val_seq, val_labels),
+                epochs=20, callbacks=[tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=2)])
 
-test_descriptions, test_labels = get_description(test)
-test_seq = get_sequences(tokenizer, test_descriptions)
-test_labels = names_to_ids(test_labels)
-model.evaluate(test_seq, test_labels)
+    test_descriptions, test_labels = get_description(test)
+    test_seq = get_sequences(tokenizer, test_descriptions)
+    test_labels = names_to_ids(test_labels)
+    accuracy = model.evaluate(test_seq, test_labels)[1]
+    results.append({'round': b, 'accuracy': accuracy})
+
+print(results)
+with open('results.json', 'w') as f:
+    json.dump(results, f)
