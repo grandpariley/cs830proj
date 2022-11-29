8021e41fc5a5b4b04b186f13ee8053e3a5fc54da
grandpariley <rileyherman@tuta.io>
2022-11-28 21:52:09 -0600

    ooooh pretty graphs

--- results.py
+++ results.py
@@ -1,4 +1,6 @@
 import json
+import matplotlib.pyplot as plt
+
 results = {
     'classic': {
         'graph': [],
@@ -9,13 +11,36 @@ results = {
         'margin': []
     }
 }
-with open('classiclearning/results-graph.py', 'r') as f:
+with open('classiclearning/results-graph.json', 'r') as f:
     results['classic']['graph'] = json.load(f)
-with open('classiclearning/results-margin.py', 'r') as f:
+with open('classiclearning/results-margin.json', 'r') as f:
     results['classic']['margin'] = json.load(f)
-with open('deeplearning/results-graph.py', 'r') as f:
+with open('deeplearning/results-graph.json', 'r') as f:
     results['deep']['graph'] = json.load(f)
-with open('deeplearning/results-margin.py', 'r') as f:
+with open('deeplearning/results-margin.json', 'r') as f:
     results['deep']['margin'] = json.load(f)
 
 print(results)
+
+def graph(index, x, y, title): 
+    plt.subplot(2, 2, index)
+    plt.scatter(x, y)
+    plt.title(title)
+    plt.xlabel('Batch')
+    plt.ylabel('Accuracy')
+
+x = [r['round'] for r in results['classic']['graph']]
+y = [r['accuracy'] for r in results['classic']['graph']]
+graph(1, x, y, 'SVM using Graph Density Active Learning')
+x = [r['round'] for r in results['classic']['margin']]
+y = [r['accuracy'] for r in results['classic']['margin']]
+graph(2, x, y, 'SVM using Margin Active Learning')
+x = [r['round'] for r in results['deep']['graph']]
+y = [r['accuracy'] for r in results['deep']['graph']]
+graph(3, x, y, 'Neural Network using Graph Density Active Learning')
+x = [r['round'] for r in results['deep']['margin']]
+y = [r['accuracy'] for r in results['deep']['margin']]
+graph(4, x, y, 'Neural Network using Margin Active Learning')
+
+plt.tight_layout()
+plt.show()
